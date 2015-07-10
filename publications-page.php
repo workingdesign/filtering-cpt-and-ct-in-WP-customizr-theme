e8081485a4b86cf93b51bc351e4bbf57642ddd3b
Giorgio Riccardi <griccardi@my.bcit.ca>
2015-07-10 08:00:39 -0700

    added extra strings [as suggested by Andy C.] to try to solve the problem, they did not work!

--- publications-page.php
+++ publications-page.php
@@ -64,6 +64,9 @@ function print_page_title(){
                                 'reports'           => $term->slug,
                                 'post_status'       => 'publish',
                                 'posts_per_page'    => 2,   //show n posts
+                                //'taxonomy'          => 'publication-types',
+                                //'term'              => $term->slug,
+                                //'term'              => 'reports',
                                 //'cat'               => 77, //include this category from the posts list
                                 //'tag_id'            => '77',
                                 //'tag'               => 'reports',
